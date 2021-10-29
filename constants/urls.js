aea5a5833b3817f9cdb205eb6c28daf08d431884
Alice Ma <alicema@berkeley.edu>
2021-10-29 15:28:29 -0400

    change url

--- constants/urls.js
+++ constants/urls.js
@@ -2,4 +2,4 @@ export const FAQ_URL =
   "https://madrealities.notion.site/FAQ-d6308bae12e9468cb81f988cb8d4bed7";
 export const DISCORD_INVITE_URL = "https://discord.gg/Xhqu8E67ct";
 export const CROWDFUND_URL =
-  "https://madrealities.notion.site/Mad-Realities-Crowdfund-b88d8709afd24ebc9e47ef33982f4f6e";
+  "https://madrealities.mirror.xyz/crowdfunds/0xCCac1187F4439E6ff02De97B16fF40BD2E7c8080";
