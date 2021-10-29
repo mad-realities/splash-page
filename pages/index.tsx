9f57a83307c3124a729565bd032c977365aa6a88
Alice Ma <alicema@berkeley.edu>
2021-10-29 01:33:22 -0400

    QUICK: switch order of meta tags ????

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,11 @@ const Home: NextPage = () => {
       <Head>
         <title>mad realities</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image" content="/cover-photo.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="/cover-photo.png" />
         <meta name="twitter:title" content="mad realities" />
         <meta name="twitter:description" content="a new interactive dating show that anyone can participate in, powered by crypto" />
+        <meta property="og:image" content="/cover-photo.png" />
       </Head>
       <GlitterBackground>
         <NavyBackground />
