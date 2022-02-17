c2697dbdd58c26873feb584cb1a9c3eaf7a859bb
Michael Boyle <mike@clubhouse.cc>
2022-02-16 19:41:13 -0500

    Open POL in new window

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
           IT’S TIME FOR
         </p>
 
-        <a href={PROOF_URL} className="proofImage"><img src="/proofoflove_logo.png" width="436" height="312" className="proofLogo" /></a>
+        <a href={PROOF_URL} className="proofImage" target="_blank"><img src="/proofoflove_logo.png" width="436" height="312" className="proofLogo" /></a>
         <img src="/new-show-dropped.png" width="622" height="622" className="starburst" />
 
         <p className="worldsFirst">the world’s <em>first interactive</em> <span className="colorPurple">dating show</span> where the community decides what happens (and who happens).</p>
